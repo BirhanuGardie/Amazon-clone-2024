cfac26fea315f7d31b7a33928c39c27c46b1ac51
BirhanuGardie <106751124+BirhanuGardie@users.noreply.github.com>
2024-12-03 11:53:41 +0300

    Carousel img update

---
+++ Amazon-clone/src/Components/Category/CategoryCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CategoryCard() {
+  return (
+    <div>
+      <a href="">
+        <span>
+            <h2>title</h2>
+        </span>
+        <img src="" alt="" />
+      </a>
+    </div>
+  );
+}
+
+export default CategoryCard;
