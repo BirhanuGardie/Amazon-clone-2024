ca1b2226f10289a04525b02e41b2c7ab1f89617f
BirhanuGardie <106751124+BirhanuGardie@users.noreply.github.com>
2024-12-10 11:21:09 +0300

    detail page routing

--- Amazon-clone/src/pages/ProductDetail/ProductDetail.jsx
+++ Amazon-clone/src/pages/ProductDetail/ProductDetail.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from './productDetail.module.css'
 import Layout from '../../Components/Layout/Layout'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { productUrl } from '../../Api/endpoint'
+import ProductCard from '../../Components/Product/ProductCard'
 
 function ProductDetail() {
+  const {productId}=useParams()
+  const[product, setproduct]= useState({})
+  useEffect(()=>{
+    axios.get(`${productUrl}/products/${productId}`)
+    .then((res)=>{
+      setproduct(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+
+  }, [])
   return (
     <Layout>
-    <div>Product Details</div>
+    <ProductCard product= {product} />
     </Layout>
   )
 }
